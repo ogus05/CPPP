35016480de3bfa985df38932995870e4167eae44
ogus05 <ogus05@naver.com>
2021-01-30 14:00:20 +0900

    add gitignore

--- first.cpp
+++ first.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
 
 using namespace std;
 
 int main(){
-    cout << "Hello, world!" << endl;
 
-    cout << "Hello world2" << endl;
-    return 0;
+    mutex mtx;  //mutual exclusion
+
+    auto work_func = [&](const string & name){
+        for(int i = 0;i<5;i++){
+            this_thread::sleep_for(chrono::milliseconds(100));
+            mtx.lock();
+            cout << name << " " << this_thread::get_id() << " is working" << endl;
+            mtx.unlock();
+        }
+    };
+
+    thread t1 = thread(work_func, "JackJack");
+    thread t2 = thread(work_func, "Dash");
+
+    t1.join();
+    t2.join();
 }
